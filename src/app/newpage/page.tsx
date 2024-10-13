c89a23484e7dea0dbdc97b3285e36ebf1f7bf5a2
Nam Tran Nguyen <trannguyen.0710991@gmail.com>
2024-10-13 00:12:28 -0400

    finished the log in page

--- src/app/newpage/page.tsx
+++ src/app/newpage/page.tsx
@@ -1,13 +1,47 @@
-// app/newpage/page.tsx
-
+"use client";
 import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+import { Button } from '@mui/material';
+import Image from 'next/image';
+import waveLinkLogo from '../assets/waveLinkLogo.png';
+
+const StyledLoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; /* Full height of the viewport */
+  padding: 100px;
+  background-color: whitesmoke; /* Similar background to the container */
+  border-radius: 7px;
+`;
+
+const StyledImageWrapper = styled.div`
+  margin-bottom: 50px;
+`;
 
 const NewPage: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to the New Page</h1>
-      <p>This page is created using TypeScript in Next.js!</p>
-    </div>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <StyledLoginContainer>
+        <StyledImageWrapper>
+          <Image src={waveLinkLogo} alt="Wavelink Logo" height={200} width={200} />
+        </StyledImageWrapper>
+
+        <Button
+          variant="outlined"
+          onClick={() => {
+            console.log('SIGNING IN');
+          }}
+        >
+          Sign in with Google
+        </Button>
+      </StyledLoginContainer>
+    </>
   );
 };
 
